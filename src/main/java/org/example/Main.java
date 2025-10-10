1a69af096ffabee12affad50d2c24483055669de
annelis sacalxot <annejysch@gmail.com>
2025-10-09 22:03:52 -0600

    Modificacion en clase transaccion y main, del paquete transaccion, aun no completado...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,23 @@
 package org.example;
 
 import org.example.cliente.Cliente;
+import org.example.transaccion.TipoTransaccion;
+import org.example.transaccion.Transaccion;
+
+import java.time.LocalDateTime;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //pruba de llamar a cliente
+        //LLAMANDO A CLASE CLIENTE
         Cliente cliente = new Cliente("P2145","Richard Parker");
         System.out.println(cliente.toString());
+
+        //LLAMANDO A CLASE TRANSACCION
+        Transaccion trans = new Transaccion(TipoTransaccion.DEPOSITO, 78.9, LocalDateTime.now(),"P100",10);
+        System.out.println(trans.toString());
+
     }
+
 }
--- src/main/java/org/example/transaccion/Transaccion.java
+++ src/main/java/org/example/transaccion/Transaccion.java
@@ -1,4 +1,63 @@
 package org.example.transaccion;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Transaccion {
+    //atributos final porque no se puede modificar diante la ejecucion
+    private final TipoTransaccion tipoTransaccion;
+    private final double montoInvolucrado;
+    private final LocalDateTime fechaHora;
+    private final String idCuenta;
+    private final double saldoResultante;
+
+    //constructor
+
+    public Transaccion(TipoTransaccion tipoTransaccion, double montoInvolucrado, LocalDateTime fechaHora, String idCuenta, double saldoResultante) {
+        this.tipoTransaccion = tipoTransaccion;
+        this.montoInvolucrado = montoInvolucrado;
+        this.fechaHora = LocalDateTime.now();
+        this.idCuenta = idCuenta;
+        this.saldoResultante = saldoResultante;
+    }
+
+    //metodo
+
+
+    //getter and setter
+
+    public TipoTransaccion getTipoTransaccion() {
+        return tipoTransaccion;
+    }
+
+    public double getMontoInvolucrado() {
+        return montoInvolucrado;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public String getIdCuenta() {
+        return idCuenta;
+    }
+    //no incluyo set porque el id es permanenete
+
+    public double getSaldoResultante() {
+        return saldoResultante;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Transaccion{" +
+                "tipoTransaccion=" + tipoTransaccion +
+                ", montoInvolucrado=" + montoInvolucrado +
+                ", fechaHora=" + fechaHora +
+                ", idCuenta='" + idCuenta + '\'' +
+                ", saldoResultante=" + saldoResultante +
+                '}';
+    }
 }
